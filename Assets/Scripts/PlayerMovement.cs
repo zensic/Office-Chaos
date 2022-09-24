335149a5a52bc28181376840f1feb60707cbe89a
Anthony Lee <62092980+zensic@users.noreply.github.com>
2022-09-25 02:48:00 +0800

    player turning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
   public float moveSpeed = 5f;
+  public float rotationSpeed = 720;
   public Rigidbody2D rb;
 
   Vector2 movement;
@@ -19,5 +20,15 @@ public class PlayerMovement : MonoBehaviour
   void FixedUpdate()
   {
     rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+    Vector2 movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+    if (movementDirection != Vector2.zero)
+    {
+      Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+      transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+    }
+
   }
+
 }
